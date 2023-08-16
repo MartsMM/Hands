7f32eab2bf0581f07be7ca0259b4ec52fffbc16b
Marts Mikelsons <Martsm13@gmail.com>
2023-08-16 15:30:20 +0100

    Adds hammer podium and distance grabbable hammer

--- Assets/Demo/Scripts/PokeRadiusSizeMatch.cs
+++ Assets/Demo/Scripts/PokeRadiusSizeMatch.cs
@@ -9,12 +9,18 @@ public class PokeRadiusSizeMatch : MonoBehaviour
     [SerializeField] private PokeInteractor _pokeInteractor;
 
     private float _ratio = 1f;
+    private float _prevScale;
 
     private void Start(){
+        _prevScale = _objectToMatch.localScale.x;
         _ratio = _objectToMatch.localScale.x / _pokeInteractor.Radius;
     }
-
+    
+    //change this to an onResize event so we can get rid of prevScale.
     private void FixedUpdate(){
+        if (_prevScale == _objectToMatch.localScale.x) return;
+        
         _pokeInteractor.SetRadius(_objectToMatch.localScale.x / _ratio);
+        _prevScale = _objectToMatch.localScale.x;
     }
 }
